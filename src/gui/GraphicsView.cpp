861b17d7354934f7cf6bb6cc672f0941a521abfe
Arlen Avakian <arlen.prime@gmail.com>
2024-12-05 14:33:34 -0600

    gui: added GraphicsView that supports zooming, panning, and scene bookmarking.

---
+++ src/gui/GraphicsView.cpp
@@ -0,0 +1,231 @@
+#include "GraphicsView.hpp"
+#include "core/nodes.hpp"
+#include "core/Scene.hpp"
+
+#include <QMouseEvent>
+#include <QVariantAnimation>
+
+
+using namespace gui;
+
+GraphicsView::GraphicsView(QGraphicsScene *scene, QWidget *parent)
+    : QGraphicsView(scene, parent)
+{
+    configure();
+}
+
+void GraphicsView::requestSceneBookmark()
+{
+    if (_bookmarkAnimation) {
+        destroyBookmarkAnimation();
+    } else {
+        _bookmarkAnimation = new QVariantAnimation(this);
+        _bookmarkAnimation->setDuration(250);
+        _bookmarkAnimation->setStartValue(QSize(16, 16));
+        _bookmarkAnimation->setEndValue(QSize(64, 64));
+        _bookmarkAnimation->setLoopCount(-1);
+        _bookmarkAnimation->setEasingCurve(QEasingCurve::OutCubic);
+        connect(_bookmarkAnimation, &QVariantAnimation::valueChanged, this,
+            [this]{ viewport()->update(); });
+        _bookmarkAnimation->start();
+    }
+}
+
+void GraphicsView::enterEvent(QEnterEvent* event)
+{
+    togglePanOrZoom(Qt::NoModifier);
+
+    QGraphicsView::enterEvent(event);
+}
+
+void GraphicsView::keyPressEvent(QKeyEvent *event)
+{
+    togglePanOrZoom(event->modifiers());
+
+    QGraphicsView::keyPressEvent(event);
+}
+
+void GraphicsView::keyReleaseEvent(QKeyEvent* event)
+{
+    togglePanOrZoom(event->modifiers());
+
+    QGraphicsView::keyReleaseEvent(event);
+}
+
+void GraphicsView::leaveEvent(QEvent* event)
+{
+    togglePanOrZoom(Qt::NoModifier);
+
+    QGraphicsView::leaveEvent(event);
+}
+
+void GraphicsView::mouseMoveEvent(QMouseEvent* event)
+{
+    togglePanOrZoom(event->modifiers());
+    if (event->modifiers() == Qt::AltModifier && _bookmarkAnimation == nullptr) {
+        zoom();
+    }
+
+    saveMousePosition(event->pos());
+
+    QGraphicsView::mouseMoveEvent(event);
+}
+
+void GraphicsView::mousePressEvent(QMouseEvent *event)
+{
+    if (_bookmarkAnimation) {
+        destroyBookmarkAnimation();
+        addSceneBookmark(event->pos());
+    }
+
+    QGraphicsView::mousePressEvent(event);
+}
+
+void GraphicsView::mouseReleaseEvent(QMouseEvent *event)
+{
+    QGraphicsView::mouseReleaseEvent(event);
+}
+
+void GraphicsView::paintEvent(QPaintEvent *event)
+{
+    QGraphicsView::paintEvent(event);
+
+    if (_bookmarkAnimation) {
+        QPainter p(viewport());
+        drawBookmarkAnimation(p);
+    }
+}
+
+void GraphicsView::configure()
+{
+    setFrameShape(NoFrame);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setDragMode(QGraphicsView::NoDrag);
+
+    setMouseTracking(true);
+    setTransformationAnchor(QGraphicsView::NoAnchor);
+
+    // Setting background color to Qt::NoBrush is needed so that
+    // Scene::drawBackground() virtual method gets called.
+    setBackgroundBrush(Qt::NoBrush);
+
+    setProperty(MOUSE_POSITION_PROPERTY, QPoint(0, 0));
+    setProperty(MOUSE_LAST_POSITION_PROPERTY, QPoint(0, 0));
+}
+
+QPoint GraphicsView::mouseMoveVelocity() const
+{
+    const auto last = property(MOUSE_LAST_POSITION_PROPERTY).toPoint();
+    const auto curr = property(MOUSE_POSITION_PROPERTY).toPoint();
+
+    return curr - last;
+}
+
+QPoint GraphicsView::mousePosition() const
+{
+    return property(MOUSE_POSITION_PROPERTY).toPoint();
+}
+
+void GraphicsView::saveMousePosition(const QPoint& pos)
+{
+    setProperty(MOUSE_LAST_POSITION_PROPERTY, property(MOUSE_POSITION_PROPERTY).toPoint());
+    setProperty(MOUSE_POSITION_PROPERTY, pos);
+}
+
+void GraphicsView::scaleView(qreal factor)
+{
+    setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
+
+    constexpr auto unit         = QRectF(0, 0, 1, 1);
+    constexpr auto zoomInLimit  = QRectF(0, 0, 4, 4);
+    constexpr auto zoomOutLimit = QRectF(0, 0, 0.25, 0.25);
+
+    const auto sx   = factor;
+    const auto sy   = factor;
+    const auto zoom = transform().scale(sx, sy).mapRect(unit).size();
+
+    const auto candidate = QRectF(QPointF(0, 0), zoom);
+
+    if (zoomInLimit.contains(candidate) && candidate.contains(zoomOutLimit)) {
+        scale(sx, sy);
+    }
+}
+
+void GraphicsView::zoom()
+{
+    const auto velocity = mouseMoveVelocity();
+    if (velocity.x() > 0) {
+        zoomOut();
+    } else if (velocity.x() < 0) {
+        zoomIn();
+    }
+}
+
+void GraphicsView::zoomIn()
+{
+    scaleView(1.01);
+}
+
+void GraphicsView::zoomOut()
+{
+    scaleView(1 / 1.01);
+}
+
+void GraphicsView::togglePanOrZoom(Qt::KeyboardModifiers modifiers)
+{
+    auto reset = [this] {
+        viewport()->setCursor(Qt::ArrowCursor);
+        setDragMode(QGraphicsView::NoDrag);
+    };
+
+    if (_bookmarkAnimation) {
+        reset();
+        return;
+    }
+
+    if (modifiers == Qt::AltModifier) {
+        /// zooming is enabled only when Alt modifier is active.
+        viewport()->setCursor(Qt::SizeAllCursor);
+    } else if (modifiers == Qt::ControlModifier) {
+        /// ScrollHandDrag mode is enabled only when Ctrl modifier is active.
+        setDragMode(QGraphicsView::ScrollHandDrag);
+    } else {
+        /// otherwise, set everything to normal.
+        reset();
+    }
+}
+
+void GraphicsView::destroyBookmarkAnimation()
+{
+    assert(_bookmarkAnimation);
+
+    connect(_bookmarkAnimation, &QVariantAnimation::destroyed, this,
+        [this]{ viewport()->update(); });
+    _bookmarkAnimation->stop();
+    _bookmarkAnimation->deleteLater();
+    _bookmarkAnimation = nullptr;
+}
+
+void GraphicsView::drawBookmarkAnimation(QPainter& p)
+{
+    QRect rec = QRect(QPoint(0,0), _bookmarkAnimation->currentValue().toSize());
+    const auto pos = mousePosition();
+    rec.moveCenter(pos);
+    p.setPen(QPen(QColor(32, 32, 32, 255), 2));
+    p.drawLine(QLine{rec.center(), {rec.left(), rec.center().y()}});
+    p.drawLine(QLine{rec.center(), {rec.right(), rec.center().y()}});
+    p.drawLine(QLine{rec.center(), {rec.center().x(), rec.top()}});
+    p.drawLine(QLine{rec.center(), {rec.center().x(), rec.bottom()}});
+
+    const auto coord = mapToScene(pos);
+    const auto text = QString("(%1, %2)").arg(coord.x()).arg(coord.y());
+    p.drawText(pos + QPoint(-32, 48), text);
+}
+
+void GraphicsView::addSceneBookmark(const QPoint& pos)
+{
+    if (auto* gs = qobject_cast<core::Scene*>(scene())) {
+        gs->addSceneBookmark(mapToScene(pos).toPoint(), "test");
+    }
+}
---
+++ src/gui/GraphicsView.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <QtWidgets/QGraphicsView>
+
+
+class QVariantAnimation;
+
+namespace gui
+{
+    class GraphicsView : public QGraphicsView
+    {
+        Q_OBJECT
+
+        constexpr static auto MOUSE_POSITION_PROPERTY = "MOUSE_POSITION_PROPERTY";
+        constexpr static auto MOUSE_LAST_POSITION_PROPERTY = "MOUSE_LAST_POSITION_PROPERTY";
+
+    public:
+        GraphicsView(QGraphicsScene* scene, QWidget* parent = nullptr);
+
+    public slots:
+        void requestSceneBookmark();
+
+    protected:
+        void enterEvent(QEnterEvent* event) override;
+        void keyPressEvent(QKeyEvent* event) override;
+        void keyReleaseEvent(QKeyEvent* event) override;
+        void leaveEvent(QEvent* event) override;
+        void mouseMoveEvent(QMouseEvent* event) override;
+        void mousePressEvent(QMouseEvent* event) override;
+        void mouseReleaseEvent(QMouseEvent* event) override;
+        void paintEvent(QPaintEvent* event) override;
+
+    private:
+        void configure();
+
+        QPoint mouseMoveVelocity() const;
+        QPoint mousePosition() const;
+        void saveMousePosition(const QPoint& pos);
+        void scaleView(qreal factor);
+        void zoom();
+        void zoomIn();
+        void zoomOut();
+        void togglePanOrZoom(Qt::KeyboardModifiers modifiers);
+
+        void destroyBookmarkAnimation();
+        void drawBookmarkAnimation(QPainter& p);
+        void addSceneBookmark(const QPoint& pos);
+
+        QVariantAnimation* _bookmarkAnimation = nullptr;
+    };
+}
