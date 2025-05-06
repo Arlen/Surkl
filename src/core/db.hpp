5de79e59098e3e3155145619fc7c44c63d45fd51
Arlen Avakian <arlen.prime@gmail.com>
2025-05-06 17:11:01 -0500

    core: added default DatabaseConfig values.

--- src/core/db.hpp
+++ src/core/db.hpp
@@ -6,27 +6,30 @@
 
 namespace core::db
 {
+    using namespace Qt::Literals::StringLiterals;
+
     struct DatabaseConfig
     {
         QLatin1StringView databaseName;
         QLatin1StringView connectionName;
     };
 
-    constexpr auto DB_CONNECTION_NAME      = QLatin1StringView("Surkl_db_connection");
-    constexpr auto DB_DATABASE_NAME        = QLatin1StringView("Surkl.db");
-    constexpr auto DB_TEST_CONNECTION_NAME = QLatin1StringView("Surkl_test_db_connection");
-    constexpr auto DB_TEST_DATABASE_NAME   = QLatin1StringView("Surkl_test.db");
+    constexpr auto DB_CONFIG = DatabaseConfig
+        { .databaseName   = "Surkl.db"_L1
+        , .connectionName = "Surkl_db_connection"_L1
+        };
+    constexpr auto DB_CONFIG_TEST = DatabaseConfig
+        { .databaseName   = "Surkl_test.db"_L1
+        , .connectionName = "Surkl_test_db_connection"_L1
+        };
 
-    void init(const DatabaseConfig& config =
-        { .databaseName   = DB_DATABASE_NAME
-        , .connectionName = DB_CONNECTION_NAME
-        });
+
+    void init(const DatabaseConfig& config = DB_CONFIG);
 
     bool doesTableExists(QLatin1StringView name);
 
-    inline QSqlDatabase get(QLatin1StringView connectionName = DB_CONNECTION_NAME)
+    inline QSqlDatabase get(QLatin1StringView connectionName = DB_CONFIG.connectionName)
     {
         return QSqlDatabase::database(connectionName);
     }
 }
-
