3f1276fc90c68c49f5204ebb2d5b2d7d4201a179
Arlen Avakian <arlen.prime@gmail.com>
2025-06-07 00:04:39 -0500

    core: implemented incremental save.

--- src/core/SceneStorage.cpp
+++ src/core/SceneStorage.cpp
@@ -7,114 +7,172 @@
 #include "SessionManager.hpp"
 #include "db.hpp"
 
+#include <QDir>
+#include <QSqlRecord>
+#include <QTimer>
+
 
 using namespace core;
 
 SceneStorage::SceneStorage(QObject* parent)
     : QObject(parent)
 {
+    _timer = new QTimer(this);
 
+    connect(_timer, &QTimer::timeout, this, &SceneStorage::push);
 }
 
-void SceneStorage::configure(SceneStorage* ps)
+void SceneStorage::configure()
 {
-    Q_UNUSED(ps);
+    createTable();
 
-    using namespace std::ranges;
-
-    createTables();
     Q_ASSERT(core::db::doesTableExists(NODES_TABLE));
-}
-
-void SceneStorage::createTables()
-{
-    const auto db = db::get();
-
-    if (!db.isOpen()) { return; }
-
-    QSqlQuery q(db);
-
-    q.exec(
-        QLatin1StringView(R"(CREATE TABLE IF NOT EXISTS %1
-                            ( %2 TEXT NOT NULL PRIMARY KEY
-                            , %3 TEXT
-                            , %4 INTEGER
-                            , %5 REAL
-                            , %6 REAL
-                            , %7 REAL))")
-            .arg(NODES_TABLE)
-            .arg(NODE_ID)
-            .arg(PARENT_ID)
-            .arg(NODE_STATE)
-            .arg(NODE_XPOS)
-            .arg(NODE_YPOS)
-            .arg(EDGE_LEN));
 }
 
 void SceneStorage::saveNode(const NodeItem *node)
 {
-    if (auto db = db::get(); db.isOpen()) {
-        QSqlQuery q(db);
+    Q_ASSERT(node != nullptr);
 
-        q.prepare(QLatin1StringView("INSERT OR REPLACE INTO %1 (%2,%3,%4,%5,%6,%7) VALUES(?, ?, ?, ?, ?, ?)")
-            .arg(NODES_TABLE)
+    _toBeSaved.removeOne(node);
+    _toBeSaved.push_back(node);
 
-            .arg(NODE_ID)
-            .arg(PARENT_ID)
-            .arg(NODE_STATE)
-            .arg(NODE_XPOS)
-            .arg(NODE_YPOS)
-            .arg(EDGE_LEN));
-
-        q.addBindValue(SessionManager::scene()->filePath(node->index()));
-        q.addBindValue(SessionManager::scene()->filePath(node->index().parent()));
-        q.addBindValue(static_cast<int>(node->nodeType()));
-        const auto pos = node->scenePos();
-        q.addBindValue(pos.x());
-        q.addBindValue(pos.y());
-        if (auto* pe  = node->parentEdge(); pe) {
-            q.addBindValue(pe->line().length());
-        }
-
-        if (!q.exec()) {
-            qWarning() << q.lastError();
-        }
-    }
+    _timer->start(125);
 }
 
-void SceneStorage::saveScene(const QGraphicsScene* scene)
+void SceneStorage::saveNodes(const QList<const NodeItem*>& nodes) const
 {
     if (auto db = db::get(); db.isOpen()) {
         db.transaction();
         QSqlQuery q(db);
 
-        q.prepare(QLatin1StringView("INSERT OR REPLACE INTO %1 (%2,%3,%4,%5,%6,%7) VALUES(?, ?, ?, ?, ?, ?)")
+        q.prepare(QLatin1StringView("INSERT OR REPLACE INTO %1 (%2,%3,%4,%5,%6) VALUES(?, ?, ?, ?, ?)")
             .arg(NODES_TABLE)
 
             .arg(NODE_ID)
-            .arg(PARENT_ID)
-            .arg(NODE_STATE)
+            .arg(NODE_TYPE)
             .arg(NODE_XPOS)
             .arg(NODE_YPOS)
             .arg(EDGE_LEN));
 
-        const auto items = scene->items();
-        for (auto* item : items) {
-            if (const auto* node = qgraphicsitem_cast<NodeItem*>(item); node) {
-                q.addBindValue(SessionManager::scene()->filePath(node->index()));
-                q.addBindValue(SessionManager::scene()->filePath(node->index().parent()));
+        for (auto* node : nodes) {
+            if (node && node->index().isValid()) {
+                q.addBindValue(_scene->filePath(node->index()));
                 q.addBindValue(static_cast<int>(node->nodeType()));
                 const auto pos = node->scenePos();
                 q.addBindValue(pos.x());
                 q.addBindValue(pos.y());
-                if (auto* pe  = node->parentEdge(); pe) {
+                if (const auto* pe  = node->parentEdge(); pe) {
                     q.addBindValue(pe->line().length());
+                }
+                if (!q.exec()) {
+                    qWarning() << q.lastError();
                 }
             }
         }
 
+void SceneStorage::saveScene(const QGraphicsScene* scene)
         if (!db.commit()) {
             qWarning() << q.lastError();
         }
     }
+}
+
+void SceneStorage::saveScene() const
+{
+    const auto toBeSaved = _scene->items()
+        | std::views::transform(&asNodeItem)
+        | std::ranges::to<QList<const NodeItem*>>()
+        ;
+
+    saveNodes(toBeSaved);
+}
+
+{
+    if (auto db = db::get(); db.isOpen()) {
+        db.transaction();
+void SceneStorage::push()
+{
+    if (const auto nextN = qMin(32, _toBeSaved.size()); nextN > 0) {
+        const auto nodes = _toBeSaved.first(nextN);
+        _toBeSaved.remove(0, nextN);
+        saveNodes(nodes);
+    }
+
+    if (_toBeSaved.empty()) {
+        _timer->stop();
+    }
+}
+
+void SceneStorage::enableSave()
+{
+    connect(_scene, &QGraphicsScene::changed,
+        [this](const QList<QRectF>& region) {
+            for (const auto& reg : region) {
+                for (const auto items = _scene->items(reg); auto* item : items) {
+                    if (auto* node = qgraphicsitem_cast<const NodeItem*>(item); node) {
+                        saveNode(node);
+                    }
+                }
+            }
+        });
+}
+
+void SceneStorage::createTable()
+{
+    if (const auto db = db::get(); db.isOpen()) {
+        QSqlQuery q(db);
+
+        q.exec(
+            QLatin1StringView(R"(CREATE TABLE IF NOT EXISTS %1
+                            ( %2 TEXT PRIMARY KEY
+                            , %4 INTEGER
+                            , %5 REAL
+                            , %6 REAL
+                            , %7 REAL))")
+                .arg(NODES_TABLE)
+                .arg(NODE_ID)
+                .arg(NODE_TYPE)
+                .arg(NODE_XPOS)
+                .arg(NODE_YPOS)
+                .arg(EDGE_LEN));
+    }
+}
+
+QHash<QPersistentModelIndex, QList<NodeData>> SceneStorage::readTable(const FileSystemScene* scene)
+{
+    const auto db = db::get();
+
+    if (!db.isOpen()) {
+        return {};
+    }
+
+    QHash<QPersistentModelIndex, QList<NodeData>> graph;
+
+    QSqlQuery q(db);
+    q.prepare(QLatin1StringView("SELECT * FROM %1").arg(NODES_TABLE));
+
+    if (q.exec()) {
+        const auto rec       = q.record();
+        const auto idIndex   = rec.indexOf(NODE_ID);
+        const auto typeIndex = rec.indexOf(NODE_TYPE);
+        const auto xposIndex = rec.indexOf(NODE_XPOS);
+        const auto yposIndex = rec.indexOf(NODE_YPOS);
+        const auto lenIndex  = rec.indexOf(EDGE_LEN);
+        auto ok = false;
+
+        while (q.next()) {
+            const auto path  = q.value(idIndex).toString();
+            const auto index = scene->index(path);
+            const auto type  = static_cast<NodeType>(q.value(typeIndex).toInt(&ok)); Q_ASSERT(ok);
+            const auto x     = q.value(xposIndex).toReal(&ok); Q_ASSERT(ok);
+            const auto y     = q.value(yposIndex).toReal(&ok); Q_ASSERT(ok);
+            const auto len   = q.value(lenIndex).toReal(&ok);  Q_ASSERT(ok);
+
+            if (index.isValid()) {
+                graph[index.parent()].push_back(NodeData{ index, type, QPointF(x, y), len });
+            }
+        }
+    }
+
+    return graph;
 }
--- src/core/SceneStorage.hpp
+++ src/core/SceneStorage.hpp
@@ -7,32 +7,52 @@
 
 
 class QGraphicsScene;
+class QTimer;
 
 namespace core
 {
     class NodeItem;
+    class FileSystemScene;
 
     constexpr auto NODES_TABLE = QLatin1String("Nodes");
     constexpr auto NODE_ID     = QLatin1String("node_id");
-    constexpr auto PARENT_ID   = QLatin1String("parent_id");
-    constexpr auto NODE_STATE  = QLatin1String("node_state");
+    constexpr auto NODE_TYPE   = QLatin1String("node_type");
     constexpr auto NODE_XPOS   = QLatin1String("node_xpos");
     constexpr auto NODE_YPOS   = QLatin1String("node_ypos");
     constexpr auto EDGE_LEN    = QLatin1String("edge_len");
 
 
+    class NodeData;
+
     class SceneStorage final : public QObject
     {
+        Q_OBJECT
+
     public:
         explicit SceneStorage(QObject* parent = nullptr);
 
-        static void configure(SceneStorage* ps);
+        static void configure();
 
         void saveNode(const NodeItem* node);
 
         static void saveScene(const QGraphicsScene* scene);
+        void saveNodes(const QList<const NodeItem*>& nodes) const;
+
+        void saveScene() const;
+
+
+    public slots:
+        void push();
 
     private:
-        static void createTables();
+        void enableSave();
+
+        static void createTable();
+
+        static QHash<QPersistentModelIndex, QList<NodeData>> readTable(const FileSystemScene* scene);
+
+        FileSystemScene* _scene{nullptr};
+        QTimer* _timer{nullptr};
+        QList<const NodeItem*> _toBeSaved;
     };
 }
