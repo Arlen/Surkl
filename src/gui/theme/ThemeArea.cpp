8c77ea63acbfb282086271e7f3e33480c6b06861
Arlen Avakian <arlen.prime@gmail.com>
2025-06-30 08:44:36 -0500

    theme: implemented ViewArea.

---
+++ src/gui/theme/ThemeArea.cpp
@@ -0,0 +1,30 @@
+/// Copyright (C) 2025 Arlen Avakian
+/// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "ThemeArea.hpp"
+#include "ThemeSettings.hpp"
+#include "window/TitleBar.hpp"
+
+#include <QVBoxLayout>
+
+
+using namespace gui::theme;
+
+ThemeArea::ThemeArea(window::Window *parent)
+    : AbstractWindowArea(parent)
+{
+    auto *layout = new QVBoxLayout(this);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
+
+    layout->addWidget(new ThemeSettings(this));
+
+    setFocusPolicy(Qt::StrongFocus);
+}
+
+void ThemeArea::setTitleBar(window::TitleBar *tb)
+{
+    AbstractWindowArea::setTitleBar(tb);
+
+    titleBar()->titleButton()->setText("Theme Settings");
+}
---
+++ src/gui/theme/ThemeArea.hpp
@@ -0,0 +1,20 @@
+/// Copyright (C) 2025 Arlen Avakian
+/// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "window/AbstractWindowArea.hpp"
+
+
+namespace gui::theme
+{
+    class ThemeArea final : public window::AbstractWindowArea
+    {
+        Q_OBJECT
+
+    public:
+        explicit ThemeArea(window::Window* parent = nullptr);
+
+        void setTitleBar(window::TitleBar* tb) override;
+    };
+}
